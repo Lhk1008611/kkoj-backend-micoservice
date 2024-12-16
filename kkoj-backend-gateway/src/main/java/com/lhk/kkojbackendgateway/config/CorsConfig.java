25fc90630eae05049d4552a9f0b37c98824979d6
Lhk <212792772@qq.com>
2024-12-16 17:52:05 +0800

    Feat: 新增 解决跨域配置

---
+++ kkoj-backend-gateway/src/main/java/com/lhk/kkojbackendgateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.lhk.kkojbackendgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Arrays;
+
+/**
+ * spring cloud GateWay 处理跨域
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        //此处填写允许跨域的域名，上线改为 线上真实域名 和 本地域名，不建议使用 *
+        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
